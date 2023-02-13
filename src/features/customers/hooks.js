1302386f64a32e95ea75ea7a8a44e12439392501
Ruth Temporal <ruthtemp@gmail.com>
2023-02-13 18:57:22 +0100

    hooks

---
+++ src/features/customers/hooks.js
@@ -0,0 +1,48 @@
+import { useDispatch, useSelector } from "react-redux";
+import * as actions from "./reducers";
+
+export const useUpdateFields = () => {
+  const dispatch = useDispatch();
+  const fields = useSelector((state) => state.customer.customerFields);
+
+  return {
+    fields,
+    setFormField: (field) => (value) => {
+      console.log(`updating field ${field} to ${value}`);
+      return dispatch(actions.setFormField({ field, value }));
+    },
+  };
+};
+
+export const useNewCustomer = () => {
+  const dispatch = useDispatch();
+
+  return {
+    onSubmit: () => {
+      console.log("Dispatching CREATE_CUSTOMER action");
+      dispatch(actions.createCustomerStatus());
+    },
+  };
+};
+
+export const useNewCustomerStatus = () => {
+  return useSelector((state) => {
+    console.log("status", state.customer.create.status);
+    // debugger;
+    return state.customer.create.status;
+  });
+};
+
+export const useEditCustomer = () => {
+  const dispatch = useDispatch();
+  return {
+    onSubmit: () => {
+      console.log("Dispatching EDIT_CUSTOMER action");
+      dispatch(actions.editCustomerResult());
+    },
+  };
+};
+
+export const useListCustomers = () => {
+  return useSelector((state) => state.customer.list.customers);
+};
