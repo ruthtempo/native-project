c407d6894b13ef2d3c4d58da748da6ed30cc777b
Ruth Temporal <ruthtemp@gmail.com>
2023-02-15 22:02:07 +0100

    adds sagas to handle editCustomer async

--- src/features/customers/sagas/index.js
+++ src/features/customers/sagas/index.js
@@ -4,11 +4,13 @@ import {
   watchCreateCustomer,
   watchGetExistingCustomers,
 } from "./create";
+import { watchEditCustomer } from "./edit";
 
 export default function* customer() {
   yield all([
     watchCreateCustomer(),
     watchGetExistingCustomers(),
     watchClearStorage(),
+    watchEditCustomer(),
   ]);
 }
