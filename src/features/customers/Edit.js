da0c09d74d44a89024e75df8ccac478732e60496
Ruth Temporal <ruthtemp@gmail.com>
2023-02-17 13:22:13 +0100

    pass status prop to Form

--- src/features/customers/Edit.js
+++ src/features/customers/Edit.js
@@ -5,6 +5,9 @@ import { useEditCustomer } from "./hooks";
 export const Edit = () => {
   const { params } = useRoute();
   const { customerID } = params;
-  const { onSubmit } = useEditCustomer(customerID);
-  return <Form handleSubmit={onSubmit} customerID={customerID} />;
+  const { onSubmit, status } = useEditCustomer(customerID);
+
+  return (
+    <Form handleSubmit={onSubmit} customerID={customerID} status={status} />
+  );
 };
