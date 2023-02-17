ca3b4758ba26c99a18098e476405cbe1d8188635
Ruth Temporal <ruthtemp@gmail.com>
2023-02-17 13:23:48 +0100

    returns coppy of fields

--- src/features/customers/sagas/edit.js
+++ src/features/customers/sagas/edit.js
@@ -1,6 +1,5 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { call, put, select, takeLatest } from "redux-saga/effects";
-import customer from ".";
 import * as actions from "../reducers";
 
 export function* watchEditCustomer() {
@@ -9,15 +8,16 @@ export function* watchEditCustomer() {
 
 function* takeEditCustomer(action) {
   console.log("Starting fetch request to API -- EDIT");
-  const customerID = action.payload;
+
+  const customerID = action.payload; //id
 
   try {
     const fields = yield select((state) => state.customer.customerFields);
     const customers = yield select((state) => state.customer.list.customers);
 
     const result = customers.map((cust) => {
-      if (cust.id !== customerID) return customer;
-      return fields;
+      if (cust.id !== customerID) return cust;
+      return { ...fields };
     });
 
     yield call(
