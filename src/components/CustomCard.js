5e724627553486633718ddd949199ab562b0840e
Ruth Temporal <ruthtemp@gmail.com>
2023-02-17 13:17:27 +0100

    custom card wrapped in view

--- src/components/CustomCard.js
+++ src/components/CustomCard.js
@@ -1,14 +1,16 @@
-import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { Text, StyleSheet, TouchableOpacity, View } from "react-native";
 
 export const CustomCard = ({ item, onPress }) => {
   return (
-    <TouchableOpacity style={styles.card} onPress={onPress}>
-      <Text>ID: {item.id}</Text>
-      <Text>First Name: {item.firstName}</Text>
-      <Text> Last Name: {item.lastName}</Text>
-      <Text> Active ? {item.active ? "active" : "inactive"}</Text>
-      <Text> Region: {item.region}</Text>
-    </TouchableOpacity>
+    <View>
+      <TouchableOpacity style={styles.card} onPress={onPress}>
+        <Text>ID: {item.id}</Text>
+        <Text>First Name: {item.firstName}</Text>
+        <Text> Last Name: {item.lastName}</Text>
+        <Text> Active ? {item.active ? "active" : "inactive"}</Text>
+        <Text> Region: {item.region}</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
