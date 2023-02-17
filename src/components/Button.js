ed5b34f083600c18888f54e9fc506007eb684b8b
Ruth Temporal <ruthtemp@gmail.com>
2023-02-17 13:17:08 +0100

    styles custom button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,36 +1,50 @@
 import { Text, StyleSheet, Pressable, View } from "react-native";
 
-export const CustomButton = ({ text, onPress }) => {
+export const CustomButton = ({ text, onPress, disabled }) => {
+  const styles = stylesFn({ disabled: disabled });
   return (
     <View style={styles.buttonContainer}>
-      <Pressable onPress={onPress} style={styles.button}>
+      <Pressable onPress={onPress} style={styles.button} disabled={disabled}>
         <Text style={styles.buttonLabel}>{text}</Text>
       </Pressable>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  buttonContainer: {
-    width: 320,
-    height: 68,
-    marginHorizontal: 20,
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 3,
-    alignSelf: "center",
-  },
-  button: {
-    borderRadius: 30,
-    width: "100%",
-    height: "100%",
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "row",
-    backgroundColor: "purple",
-  },
-  buttonLabel: {
-    color: "#fff",
-    fontSize: 20,
-  },
-});
+export const stylesFn = ({ disabled = false }) => {
+  let backgroundColor;
+  let color;
+
+  if (disabled) {
+    backgroundColor = "lightgray";
+    color = "grey";
+  } else {
+    backgroundColor = "purple";
+    color = "white";
+  }
+
+  return StyleSheet.create({
+    buttonContainer: {
+      width: 320,
+      height: 68,
+      marginHorizontal: 20,
+      alignItems: "center",
+      justifyContent: "center",
+      padding: 3,
+      alignSelf: "center",
+    },
+    button: {
+      borderRadius: 30,
+      width: "100%",
+      height: "100%",
+      alignItems: "center",
+      justifyContent: "center",
+      flexDirection: "row",
+      backgroundColor: backgroundColor,
+    },
+    buttonLabel: {
+      color: color,
+      fontSize: 20,
+    },
+  });
+};
